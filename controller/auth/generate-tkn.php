b78ffe132e6b67ce332eba4767672ace8a9d0ff7
Eliot Valdes <eliottvaldes@hotmail.com>
2023-02-23 15:56:51 -0600

    add -> create temporal f' to generate a user tkn

---
+++ controller/auth/generate-tkn.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Generate a tkn for a user using the uid
+ *
+ * @param integer $uid
+ * @return string
+ */
+function generateTkn(int $uid): string
+{
+    // here the logic to generate a tkn and return it
+    // your code here
+
+    return $uid;
+}
