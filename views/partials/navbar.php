c98c71a28a9e69dab4b07ae2e9a1ad7946af874c
Eliot Valdes <eliottvaldes@hotmail.com>
2023-02-23 17:23:31 -0600

    add -> create navbar partial to include in all pages

---
+++ views/partials/navbar.php
@@ -0,0 +1,9 @@
+<?php
+
+$path = '/escom-project';
+?>
+<nav class="nav justify-content-center bg-dark ">
+    <a class="nav-link" href="<?= $path ?>/users/all.php">All Users</a>
+    <a class="nav-link" href="<?= $path ?>/users/create.php">Create User</a>
+    <a class="nav-link" href="<?= $path ?>/auth/signin.php">Signin User</a>
+</nav>
